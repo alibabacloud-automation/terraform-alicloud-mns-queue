1447e2dc6f558193bec762d38aa16a9ca030cb33
shanye <shanye.xzq@alibaba-inc.com>
2024-05-06 14:12:50 +0800

    examples/complete: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,6 +1,11 @@
+resource "random_integer" "default" {
+  min = 10000
+  max = 99999
+}
+
 module "example" {
   source                   = "../.."
-  name                     = "tf-example"
+  name                     = "tf-example-${random_integer.default.result}"
   delay_seconds            = var.delay_seconds
   maximum_message_size     = var.maximum_message_size
   message_retention_period = var.message_retention_period
